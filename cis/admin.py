55dc1d84eba65577aceec19ec6457e8cb1ccc26e
Kolombet Ivan <kolombet@ostrovok.ru>
2014-08-06 10:44:12 +0400

    Simple admin + fixed tastypie charset version

---
+++ cis/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from cis.models import User
+
+admin.site.register(User)
---
+++ hr/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from hr.models import Position
+
+admin.site.register(Position)
